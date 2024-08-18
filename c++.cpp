ea247c7d2638c9e709c89816a85599cb19363702
天雷 <litianlei208271@163.com162710463+litianlei208271@users.noreply.github.com>
2024-08-18 17:28:43 +0800

    first commit

---
+++ c++.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+void show()
+{
+    cout<<" dawdawdawd\n";
+}
+typedef struct student//添加了一个学生结构体赋予了最基本的信息存储功能！
+{
+    char name[16];
+    int age;
+    int wage;
+    double student_number;
+    student():name("NULLBody"),age(0),wage(0),student_number(0){}
+    void name_change()
+    {
+        cout<<"请输入你的姓名：";
+        cin.getline(name,16);
+        this->age=18;
+        this->student_number=11;
+        this->wage=18000;
+    }
+    void show()
+    {
+        cout<<student_number<<":"<<name<<":"<<age<<" 工资 "<<wage<<endl;
+    }
+}studnet;
+int main()
+{
+    show();
+    int a;
+    cin>>a;
+    studnet c;
+    c.show();
+    c.name_change();
+    c.show();
+    cout<<a<<":"<<"23\n";
+}
